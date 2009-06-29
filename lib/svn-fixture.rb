37372b687e740830611d345425c69fab0568c551
Jared Morgan <jmorgan@morgancreative.net>
2009-06-28 19:08:20 -0500

    Update SvnFixture#svn_time to clean up code, add comments and specs
    - Also changed handling of a non-Time/Date argument to call Time.parse on it, instead of just returning the value.

--- lib/svn-fixture.rb
+++ lib/svn-fixture.rb
@@ -1,23 +1,28 @@
 require "fileutils"
+require "tmpdir"
+require "date"
+require "time"
+# Subversion Ruby bindings must be installed (see README)
 require "svn/core"
 require "svn/fs"
 require "svn/repos"
 
 module SvnFixture
-
-  def self.svn_time(val)
-    if val.respond_to?(:strftime)
-      return val.strftime("%Y-%m-%dT%H:%M:%S.000000Z")
+  class << self
+    # Return time string formatted as expected by ::Svn::Client::Context#propset
+    # (example 2009-06-28T12:00:00.000000Z). If +val+ does not respond to 
+    # +strftime+, val will first be parsed via +Time.parse+.
+    def svn_time(val)
+      val = Time.parse(val) unless val.respond_to?(:strftime)
+      val.strftime("%Y-%m-%dT%H:%M:%S.000000Z")
     end
-    return val
+    
+    def repo(name, repos_path = nil, &block)
+      r = SvnFixture::Repository.get(name, repos_path)
+      r.instance_eval(&block) if block_given?
+      r
+    end
   end
-
-  def self.repo(name, repos_path = nil, &block)
-    r = SvnFixture::Repository.get(name, repos_path)
-    r.instance_eval(&block) if block_given?
-    r
-  end
-
 end
 
 # Require classes
--- spec/svn-fixture_spec.rb
+++ spec/svn-fixture_spec.rb
@@ -1,7 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "SvnFixture" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe SvnFixture do
+  
+  describe '.svn_time' do
+    it 'should format as expected by ::Svn::Client::Context#propset' do
+      t = Time.parse('2009-06-18 13:00')
+      SvnFixture.svn_time(t).should == '2009-06-18T13:00:00.000000Z'
+      
+      d = Date.parse('2009-06-19')
+      SvnFixture.svn_time(d).should == '2009-06-19T00:00:00.000000Z'
+    end
+    
+    it 'should parse the #to_s value if not a Date or Time' do
+      t = '2009-06-18 13:00'
+      SvnFixture.svn_time(t).should == '2009-06-18T13:00:00.000000Z'
+    end
   end
 end
