497a8aea2eac319130558cf3b4901e4832a2263c
Jared Morgan <jmorgan@morgancreative.net>
2009-06-28 19:36:45 -0500

    Add SvnFixture.config method for accessing configuration hash

--- lib/svn-fixture.rb
+++ lib/svn-fixture.rb
@@ -8,7 +8,21 @@ require "svn/fs"
 require "svn/repos"
 
 module SvnFixture
+  CONFIG_DEFAULTS = {
+    :base_path => File.join(Dir.tmpdir, 'svn-fixture')
+  }
+  
   class << self
+    # SvnFixture::config method returns Hash that can be edited.
+    # The only current option is
+    # +:base_path+ : The path at which repositories are created. It default
+    # to the OS tmp directory, plus "svn-fixture". For example, 
+    # "/tmp/svn-fixture". The repo name is then appended in 
+    # +SvnFixture::Repository+.
+    def config
+      @config ||= CONFIG_DEFAULTS.dup
+    end
+    
     # Return time string formatted as expected by ::Svn::Client::Context#propset
     # (example 2009-06-28T12:00:00.000000Z). If +val+ does not respond to 
     # +strftime+, val will first be parsed via +Time.parse+.
@@ -23,6 +37,11 @@ module SvnFixture
       r
     end
   end
+end
+
+if defined?(Merb::Plugins)
+  # Make config accessible through Merb's Merb::Plugins.config hash
+  Merb::Plugins.config[:svn_fixture] = SvnFixture.config
 end
 
 # Require classes
---
+++ spec/svn-fixture/config_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe "SvnFixture.config" do
+  before(:each) do
+    # Force to default state
+    SvnFixture.instance_variable_set(:@config, nil)
+  end
+  
+  after(:all) do
+    # Force to default state for other specs
+    SvnFixture.instance_variable_set(:@config, nil)
+  end
+  
+  it 'should initialize with CONFIG_DEFAULTS' do
+    SvnFixture.config.should == SvnFixture::CONFIG_DEFAULTS
+  end
+  
+  it 'should be writable' do
+    SvnFixture.config[:base_path] = '/tmp/elsewhere'
+    SvnFixture.instance_variable_get(:@config)[:base_path].
+        should == '/tmp/elsewhere'
+  end
+  
+  describe ':base_path' do
+    it 'should default to "#{Dir.tmpdir)/#{svn-fixture}"' do
+      SvnFixture.config[:base_path].should == File.join(Dir.tmpdir, 'svn-fixture')
+    end
+  end
+end
