f24c118ecbb490a293e7a5d30b20de459ce9e38e
Jared Morgan <jmorgan@morgancreative.net>
2009-07-03 19:47:48 -0500

    Add comments and specs for File class

--- lib/svn-fixture/file.rb
+++ lib/svn-fixture/file.rb
@@ -1,13 +1,37 @@
 module SvnFixture
+  # A File to be added to the Repository. Normally, this would done through
+  # Revison#file (or Directory#file), in a block, for example:
+  #
+  #     SvnFixture.repo('repo_name') do
+  #       revision(1, 'msg') do
+  #         file('file.txt') do
+  #           prop('name', 'value')
+  #           body('Some Text')
+  #         end
+  #       end
+  #     end
+  #
+  # In that case, Revision takes care of passing the +ctx+ argument.
   class File
+    
+    # +new+ is normally called through Revison#file (or Directory#file).
+    #
+    # Arguments are:
+    # - +ctx+: An Svn::Client::Context, normally from Repository#ctx
+    # - +path+: The path (on the file system) of the File in the working copy
     def initialize(ctx, path)
       @ctx, @path = ctx, path
     end
     
+    # Set a property for the file
+    # (see http://svnbook.red-bean.com/en/1.1/ch07s02.html):
+    # - +name+: The property name (must be "human-readable text")
+    # - +value+: The value of the property.
     def prop(name, value)
       @ctx.propset(name, SvnFixture.svn_prop(value), @path)
     end
     
+    # Set the content of a file
     def body(val)
       ::File.open(@path, 'w') { |f| f.write(val) }
     end
---
+++ spec/svn-fixture/file_spec.rb
@@ -0,0 +1,65 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe SvnFixture::File do
+  before(:all) do
+    @klass = SvnFixture::File
+    SvnFixture::Repository.destroy_all
+    
+    # Set up repo
+    @repos = SvnFixture::repo('file_test') do
+      revision(1, 'create file') do
+        file('test.txt')
+      end
+    end
+    @repos.commit
+    @repos_path = @repos.instance_variable_get(:@repos_path)
+    @wc_path = @repos.instance_variable_get(:@wc_path)
+    @ctx = @repos.ctx
+  end
+  
+  before(:each) do
+    @path = File.join(@wc_path, 'test.txt')
+    @full_repos_path = "file://#{File.join(@repos_path, 'test.txt')}"
+    @file = @klass.new(@ctx, @path)
+  end
+  
+  after(:all) do
+    SvnFixture::Repository.destroy_all
+  end
+  
+  describe '#initialize' do
+    it 'should set ctx and path' do
+      file = @klass.new(@ctx, '/tmp/path')
+      file.instance_variable_get(:@ctx).should == @ctx
+      file.instance_variable_get(:@path).should == '/tmp/path'
+    end
+  end
+  
+  describe '#prop' do    
+    it 'should set a property' do
+      @file.prop('prop:name', 'Prop Value')
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:name', @path, rev)[@full_repos_path].should ==
+          'Prop Value'
+      
+      @file.prop('prop:name', 'New Value')
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:name', @path, rev)[@full_repos_path].should ==
+          'New Value'
+    end
+    
+    it 'should format a Time correctly' do
+      @file.prop('prop:timeval', Time.parse('2009-06-18 14:00'))
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:timeval', @path, rev)[@full_repos_path].should ==
+          '2009-06-18T14:00:00.000000Z'
+    end
+  end
+  
+  describe '#body' do
+    it "should update the File's contents" do
+      @file.body('Test Content')
+      File.read(@path).should == 'Test Content'
+    end
+  end
+end
