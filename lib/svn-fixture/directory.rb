9a5c2efbe4b176ca2dd342ebfaeb409a3f71e4b1
Jared Morgan <jmorgan@morgancreative.net>
2009-07-04 18:53:40 -0500

    Add comments and specs for Directory class

--- lib/svn-fixture/directory.rb
+++ lib/svn-fixture/directory.rb
@@ -1,20 +1,65 @@
 module SvnFixture
+  # A Directory to be added to or edited within the Repository. Normally, this 
+  # would br done through Directory#dir, in a block given to a Directory or
+  # Revision, for example:
+  #
+  #     SvnFixture.repo('repo_name') do
+  #       revision(1, 'msg') do
+  #         dir('test-dir') do
+  #           prop('name', 'value')
+  #           file('file.txt')
+  #         end
+  #       end
+  #     end
+  #
+  # In that case, Revision takes care of passing the +ctx+ argument.
+  # 
+  # To call SvnFixture::Directory.new directly, you will need to set up a 
+  # context (instance of Svn::Client::Context) and check out a working copy. 
+  # +SvnFixture.simple_context+ is a quick method for settin up a Context.
+  #
+  # Assuming an existing checked out working copy:
+  #
+  #     ctx = SvnFixture.simple_context
+  #     d = SvnFixture::Directory.new(ctx, '/full/fs/path/to/dir')
+  #     d.prop('propname', 'Value')
+  #
+  # Or, call #checkout on Context:
+  #
+  #     ctx = SvnFixture.simple_context
+  #     ctx.checkout('file:///repository/uri', '/fs/path/of/wc')
+  #     d = SvnFixture::Directory.new(ctx, '/fs/path/of/wc/to/dir')
+  #     d.prop('propname', 'Value')
   class Directory
+    
+    # +new+ is normally called through Directory#dir (a block to a Revision is
+    # applied to the root Directory).
+    #
+    # Arguments are:
+    # - +ctx+: An Svn::Client::Context, normally from Repository#ctx
+    # - +path+: The path (on the file system) of the Directory in the working 
+    #   copy.
     def initialize(ctx, path)
       @ctx  = ctx
-      @path = path + "/"
+      @path = path
+      @path += "/" unless path[-1] == 47
     end
     
+    # Create or access a subdirectory. Takes the name of the subdirectory (not a
+    # full path) and an optional block with the subdirectory as self.
     def dir(name, &block)
       path = @path + name
       unless ::File.directory?(path)
         FileUtils.mkdir_p(path)
         @ctx.add(path)
       end
-      d = Directory.new(@ctx, path)
+      d = self.class.new(@ctx, path)
       d.instance_eval(&block) if block_given?
+      d
     end
     
+    # Create or access a subdirectory. Takes the name of the file (not a
+    # full path) and an optional block with the File as self.
     def file(name, &block)
       path = @path + name
       unless ::File.file?(path)
@@ -23,22 +68,32 @@ module SvnFixture
       end
       f = File.new(@ctx, path)
       f.instance_eval(&block) if block_given?
+      f
     end
     
+    # Move a File or Directory. From should be an existing node. From and to can
+    # be any relative path below the directory.
     def move(from, to)
       @ctx.mv(@path + from, @path + to)
     end
-    
+
+    # Copy a File or Directory. From should be an existing node. From and to can
+    # be any relative path below the directory.
     def copy(from, to)
       @ctx.cp(@path + from, @path + to)
     end
     
+    # Delete (and remove from Repository) a child node.
     def delete(name)
       @ctx.delete(@path + name)
     end
     
+    # Set a property for the Directory
+    # (see http://svnbook.red-bean.com/en/1.1/ch07s02.html):
+    # - +name+: The property name (must be "human-readable text")
+    # - +value+: The value of the property.
     def prop(name, value)
       @ctx.propset(name, SvnFixture.svn_prop(value), @path[0..-2])
     end
-  end  
+  end
 end
---
+++ spec/svn-fixture/directory_spec.rb
@@ -0,0 +1,194 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe SvnFixture::Directory do
+  before(:each) do
+    @klass = SvnFixture::Directory
+    SvnFixture::Repository.destroy_all
+    
+    # Set up repo
+    @repos = SvnFixture::repo('dir_test') do
+      revision(1, 'create directory and file') do
+        dir('test-dir') do
+          dir('subdir')
+          file('file.txt')
+        end
+      end
+    end
+    @repos.commit
+    @repos_path = @repos.instance_variable_get(:@repos_path)
+    @wc_path = @repos.instance_variable_get(:@wc_path)
+    @ctx = @repos.ctx
+  end
+  
+  before(:each) do
+    @path = File.join(@wc_path, 'test-dir')
+    @full_repos_path = "file://#{File.join(@repos_path, 'test-dir')}"
+    @dir = @klass.new(@ctx, @path)
+  end
+  
+  after(:all) do
+    SvnFixture::Repository.destroy_all
+  end
+  
+  describe '#initialize' do
+    it 'should set ctx and path' do
+      dir = @klass.new(@ctx, '/tmp/path/')
+      dir.instance_variable_get(:@ctx).should == @ctx
+      dir.instance_variable_get(:@path).should == '/tmp/path/'
+    end
+    
+    it 'should add a trailing slash to path if needed' do
+      dir = @klass.new(@ctx, '/tmp/path')
+      dir.instance_variable_get(:@path).should == '/tmp/path/'
+    end
+  end
+  
+  describe '#dir' do
+    before(:each) do
+      @subdir_path = File.join(@path, 'subdir')
+    end
+    
+    it 'should create a new directory if needed (and add to Subversion)' do
+      new_path = File.join(@path, 'subdir2')
+      File.exist?(new_path).should be_false
+      new_dir = @dir.dir('subdir2')
+      @ctx.commit(@wc_path)
+      File.exist?(new_path).should be_true
+      @repos.repos.fs.root.dir?('test-dir/subdir2').should be_true
+    end
+    
+    it 'should use an existing directory' do
+      FileUtils.should_not_receive(:mkdir_p)
+      @ctx.should_not_receive(:add)
+      @dir.dir('subdir')
+    end
+    
+    it 'should setup a new SvnFixture::Directory' do
+      SvnFixture::Directory.should_receive(:new).with(@ctx, @subdir_path)
+      @dir.dir('subdir')
+    end
+    
+    it 'should return the Directory' do
+      sub = @dir.dir('subdir')
+      sub.should be_kind_of(SvnFixture::Directory)
+      sub.instance_variable_get(:@path).should == @subdir_path + '/'
+    end
+    
+    it 'should run block if given' do
+      SvnFixture::File.should_receive(:new).with(@ctx, File.join(@subdir_path, 'test.txt'))
+      @dir.dir('subdir') do
+        file('test.txt')
+      end
+    end
+  end
+  
+  describe '#file' do
+    before(:each) do
+      @file_path = File.join(@path, 'file.txt')
+    end
+    
+    it 'should create a new file if needed (and add to Subversion)' do
+      new_path = File.join(@path, 'file2.txt')
+      File.exist?(new_path).should be_false
+      f = @dir.file('file2.txt')
+      @ctx.commit(@wc_path)
+      File.exist?(new_path).should be_true
+      @repos.repos.fs.root.file?('test-dir/file2.txt').should be_true
+    end
+    
+    it 'should use an existing file' do
+      FileUtils.should_not_receive(:touch)
+      @ctx.should_not_receive(:add)
+      @dir.file('file.txt')
+    end
+    
+    it 'should setup a new SvnFixture::File' do
+      SvnFixture::File.should_receive(:new).with(@ctx, @file_path)
+      @dir.file('file.txt')
+    end
+    
+    it 'should return the File' do
+      f = @dir.file('file.txt')
+      f.should be_kind_of(SvnFixture::File)
+      f.instance_variable_get(:@path).should == @file_path
+    end
+    
+    it 'should run block if given' do
+      @dir.file('file.txt') do
+        body('Test')
+      end
+      File.read(@file_path).should == 'Test'
+    end
+  end
+  
+  describe '#move' do
+    before(:each) do
+      @file_path = File.join(@path, 'file.txt')
+    end
+    
+    it 'should move node in FS and Subversion' do
+      new_path = File.join(@path, 'subdir', 'file2.txt')
+      File.exist?(new_path).should be_false
+      File.exist?(@file_path).should be_true
+      @dir.move('file.txt', 'subdir/file2.txt')
+      File.exist?(new_path).should be_true
+      File.exist?(@file_path).should be_false
+      @ctx.commit(@wc_path)
+      @repos.repos.fs.root.file?('test-dir/file.txt').should be_false
+      @repos.repos.fs.root.file?('test-dir/subdir/file2.txt').should be_true
+    end
+  end
+  
+  describe '#copy' do
+    before(:each) do
+      @file_path = File.join(@path, 'file.txt')
+    end
+    
+    it 'should copy node in FS and Subversion' do
+      new_path = File.join(@path, 'subdir', 'file2.txt')
+      File.exist?(new_path).should be_false
+      File.exist?(@file_path).should be_true
+      @dir.copy('file.txt', 'subdir/file2.txt')
+      File.exist?(new_path).should be_true
+      File.exist?(@file_path).should be_true
+      @ctx.commit(@wc_path)
+      @repos.repos.fs.root.file?('test-dir/file.txt').should be_true
+      @repos.repos.fs.root.file?('test-dir/subdir/file2.txt').should be_true
+    end
+  end
+  
+  describe '#delete' do
+    before(:each) do
+      @file_path = File.join(@path, 'file.txt')
+    end
+    
+    it 'should copy node in FS and Subversion' do
+      File.exist?(@file_path).should be_true
+      @dir.delete('file.txt')
+      File.exist?(@file_path).should be_false
+      @ctx.commit(@wc_path)
+      @repos.repos.fs.root.file?('test-dir/file.txt').should be_false
+    end
+  end
+  
+  describe '#prop' do    
+    it 'should set a property' do
+      @dir.prop('prop:name', 'Prop Value')
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:name', @path, rev)[@full_repos_path].should ==
+          'Prop Value'
+      
+      @dir.prop('prop:name', 'New Value')
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:name', @path, rev)[@full_repos_path].should ==
+          'New Value'
+    end
+    
+    it 'should format a Time correctly' do
+      @dir.prop('prop:timeval', Time.parse('2009-06-18 14:00'))
+      rev = @ctx.ci(@wc_path).revision
+      @ctx.propget('prop:timeval', @path, rev)[@full_repos_path].should ==
+          '2009-06-18T14:00:00.000000Z'
+    end
+  end
+end
