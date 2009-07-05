a7a59725c7f3d6b41a8c5906e6d476178fe050f7
Jared Morgan <jmorgan@morgancreative.net>
2009-07-04 20:09:40 -0500

    Add comments, specs for Revision. Also, some minor changes:
    - Change @time to @date for consistency with Svn notation.
    - Different test to see if commit failed (due to no changes). Not sure if the Svn bindings changed, but what I had doesn't work.

--- lib/svn-fixture/revision.rb
+++ lib/svn-fixture/revision.rb
@@ -1,22 +1,61 @@
 module SvnFixture
+  # A Revision of the Repository. It can be added via Repository#revision to
+  # a specific Repository
+  #
+  #     repo = SvnFixture::Repository.get('test') do
+  #       revision(1, 'log msg')
+  #     end
+  #     repo.commit
+  #
+  # or specified in Repository#commit:
+  #
+  #     rev = SvnFixture::Revision.new(1, 'log msg')
+  #     SvnFixture::Repository.get('test').commit(rev)
   class Revision
     attr_reader :name
     
+    # Initialize a Revision (normally called by Repository#revision).
+    #
+    # - +name+: A name of the Revision, can be given in Array to 
+    #   Repository#commit instead of the Revision itself. Can be a revision
+    #   number, but this does not affect the actual revision number in the
+    #   Subversion repository.
+    # - +message+: Log message. Defaults to empty String.
+    # - +options+:
+    #   - +:author+: The Revision's author
+    #   - +:date+: The date and time of the commit
+    # - Optionally accepts a block. The block, if given, is run at the time
+    #   #commit is called, within the context of the root directory of the
+    #   Repository, which is an instance of SvnFixture::Directory. For example:
+    #
+    #     SvnFixture::Revision.new(1, 'log msg') do
+    #       dir('test') # Or any other SvnFixture::Directory instance method.
+    #     end
     def initialize(name, message = "", options = {}, &block)
       @name, @message, @block = name, message, block
       @author = options.delete(:author)
-      @time = SvnFixture.svn_time(options.delete(:date))
+      @date = SvnFixture.svn_time(options.delete(:date))
     end
     
+    # Processes the changes made in this revision. Normally these would be made
+    # in a block given to Revision.new. #commit runs that block against the root
+    # directory of the working copy. This method is usually called by
+    # Repository#commit instead of directly. Also sets Revision properties for
+    # log message and, optionally, author and date, based on arguments 
+    # to +.new+. If there are no changes, the commit fails and a warning to that
+    # effect.
+    # 
+    # Only argument is an instance of SvnFixture::Repository that is the
+    # Repository to which this revision is committed.
     def commit(repo)
       root = Directory.new(repo.ctx, repo.wc_path)
-      root.instance_eval(&@block)
+      root.instance_eval(&@block) if @block
       ci = repo.ctx.ci(repo.wc_path)
-      if ci # Ensure something changed
+      unless ci.revision == Svn::Core::INVALID_REVNUM
         rev = ci.revision
         repo.repos.fs.set_prop('svn:log', @message, rev) if @message
         repo.repos.fs.set_prop('svn:author', @author, rev) if @author
-        repo.repos.fs.set_prop('svn:date', @time, rev) if @time
+        repo.repos.fs.set_prop('svn:date', @date, rev) if @date
       else
         puts "Warning: No change in revision #{name} (SvnFixture::Revision#commit)"
       end
---
+++ spec/svn-fixture/revision_spec.rb
@@ -0,0 +1,70 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe SvnFixture::Revision do
+  before(:all) do
+    @klass = SvnFixture::Revision
+  end
+  
+  describe '#initialize' do
+    it 'should accept a name, message, options and block' do
+      rev = @klass.new('name', 'msg', {:author => 'author'}) do
+        dir('test-dir')
+      end
+      
+      rev.name.should == 'name'
+      rev.instance_variable_get(:@message).should == 'msg'
+      rev.instance_variable_get(:@author).should == 'author'
+      rev.instance_variable_get(:@block).should be_kind_of(Proc)
+    end
+    
+    it 'should convert +:date+ option to an +svn_time+' do
+      rev = SvnFixture::Revision.new('name', 'msg', {:date => Time.parse('2009-01-01 12:00:00Z')})
+      rev.instance_variable_get(:@date).should == '2009-01-01T12:00:00.000000Z'
+    end
+  end
+  
+  describe 'commit' do
+    before(:all) do
+      @repos = SvnFixture::repo('file_test').checkout
+      # @repos_path = @repos.instance_variable_get(:@repos_path)
+      @wc_path = @repos.instance_variable_get(:@wc_path)
+      @rev = @klass.new(1, 'msg', :author => 'author', :date => Time.parse('2009-01-01 12:00:00Z')) do
+        dir('test-dir')
+      end
+      @rev.commit(@repos)
+      @fs = @repos.repos.fs
+    end
+    
+    after(:all) do
+      SvnFixture::Repository.destroy_all
+    end
+    
+    it 'should process @block on root directory' do
+      File.exist?(File.join(@wc_path, 'test-dir')).should be_true
+    end
+    
+    it 'should commit changes' do
+      @fs.root.dir?('test-dir').should be_true
+    end
+    
+    it 'should set log message' do
+      @fs.prop(Svn::Core::PROP_REVISION_LOG, 1).should == 'msg'
+    end
+    
+    it 'should set author if given' do
+      @fs.prop(Svn::Core::PROP_REVISION_AUTHOR, 1).should == 'author'
+    end
+    
+    it 'should set date if given' do
+      @fs.prop(Svn::Core::PROP_REVISION_DATE, 1).should == 
+          Time.parse('2009-01-01T12:00:00.000000Z')
+    end
+    
+    it 'should print warning if no changes' do
+      no_change_rev = @klass.new(2, 'msg')
+      no_change_rev.should_receive(:puts).
+          with("Warning: No change in revision 2 (SvnFixture::Revision#commit)")
+      no_change_rev.commit(@repos)
+    end
+  end
+end
